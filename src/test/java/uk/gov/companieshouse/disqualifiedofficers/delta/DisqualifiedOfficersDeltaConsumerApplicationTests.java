4bae74847f537d3d379ddad3e2e2f2e545897db4
VaughanJackson123 <vjackson@companieshouse.gov.uk>
2022-03-02 16:41:47 +0000

    DSND-531: Add basic Spring Initializr generated app skeleton.

---
+++ src/test/java/uk/gov/companieshouse/disqualifiedofficers/delta/DisqualifiedOfficersDeltaConsumerApplicationTests.java
@@ -0,0 +1,13 @@
+package uk.gov.companieshouse.disqualifiedofficers.delta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DisqualifiedOfficersDeltaConsumerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

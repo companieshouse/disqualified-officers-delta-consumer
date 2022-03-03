b2cab47bc8e12074a2e66eba08f2e725d2ce936c
VaughanJackson123 <54801196+VaughanJackson123@users.noreply.github.com>
2022-03-03 14:27:42 +0000

    DSND-531: Initialise disqualified-officers-delta-consumer (#1)
    * DSND-531: Update Kafka topic name in README.
    * DSND-531: Add basic Spring Initializr generated app skeleton.
    * DSND-531: Replace tabs with 4 spaces.
    * DSND-531: Make it a web app so the app stays up.
    * DSND-531: Depend on CH parent pom.

---
+++ src/main/java/uk/gov/companieshouse/disqualifiedofficers/delta/DisqualifiedOfficersDeltaConsumerApplication.java
@@ -0,0 +1,13 @@
+package uk.gov.companieshouse.disqualifiedofficers.delta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DisqualifiedOfficersDeltaConsumerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DisqualifiedOfficersDeltaConsumerApplication.class, args);
+    }
+
+}

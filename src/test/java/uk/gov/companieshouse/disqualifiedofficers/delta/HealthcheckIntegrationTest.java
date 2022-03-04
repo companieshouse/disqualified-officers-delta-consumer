f577001b232760d72037fe89c553291123504863
VaughanJackson123 <54801196+VaughanJackson123@users.noreply.github.com>
2022-03-04 09:44:23 +0000

    DSND-531: Add health check endpoint (#2)
    * DSND-531: Add actuator dependency for health check endpoint.
    * DSND-531: Configure actuator health check endpoint URL to /healthcheck.
    * DSND-531: Add health check endpoint integration test.
    * DSND-531: Use more compact way of controlling language level in pom.
    * DSND-531: Actuator dependency must be brought in explicitly using CH parent pom.
    * DSND-531: Manage Spring Boot dependencies version.
    * DSND-531: Use later version of Surefire with CH parent pom so tests execute in Maven.

---
+++ src/test/java/uk/gov/companieshouse/disqualifiedofficers/delta/HealthcheckIntegrationTest.java
@@ -0,0 +1,26 @@
+package uk.gov.companieshouse.disqualifiedofficers.delta;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@AutoConfigureMockMvc
+@SpringBootTest
+public class HealthcheckIntegrationTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    @DisplayName("Successfully returns health status")
+    public void returnsHealthStatusSuccessfully() throws Exception {
+        mockMvc.perform(get("/healthcheck")).andExpect(status().isOk());
+    }
+
+}

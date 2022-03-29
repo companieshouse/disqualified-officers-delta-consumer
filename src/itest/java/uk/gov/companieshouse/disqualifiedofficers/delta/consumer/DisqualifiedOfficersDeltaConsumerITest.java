82cd7fb6c1826ed0d5021f5548ab46c4262f3401
bcullerton <bcullerton@companieshouse.gov.uk>
2022-03-29 10:51:30 +0100

    Add spring consumer, config and start of integration tests

---
+++ src/itest/java/uk/gov/companieshouse/disqualifiedofficers/delta/consumer/DisqualifiedOfficersDeltaConsumerITest.java
@@ -0,0 +1,24 @@
+package uk.gov.companieshouse.disqualifiedofficers.delta.consumer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.kafka.core.KafkaTemplate;
+import uk.gov.companieshouse.delta.ChsDelta;
+import uk.gov.companieshouse.disqualifiedofficers.delta.AbstractIntegrationTest;
+
+public class DisqualifiedOfficersDeltaConsumerITest extends AbstractIntegrationTest {
+
+    @Autowired
+    public KafkaTemplate<String, ChsDelta> kafkaTemplate;
+
+    @Value("${disqualified-officers.delta.topic.main}")
+    private String mainTopic;
+
+    @Test
+    public void testSendingKafkaMessage() {
+        ChsDelta chsDelta = new ChsDelta("{ \"key\": \"value\" }", 1, "some_id");
+        kafkaTemplate.send(mainTopic, chsDelta);
+    }
+
+}
---
+++ src/main/java/uk/gov/companieshouse/disqualifiedofficers/delta/config/KafkaConfig.java
@@ -0,0 +1,83 @@
+package uk.gov.companieshouse.disqualifiedofficers.delta.config;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
+import org.springframework.kafka.core.ConsumerFactory;
+import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
+import uk.gov.companieshouse.delta.ChsDelta;
+import uk.gov.companieshouse.disqualifiedofficers.delta.serialization.ChsDeltaDeserializer;
+import uk.gov.companieshouse.kafka.producer.Acks;
+import uk.gov.companieshouse.kafka.producer.CHKafkaProducer;
+import uk.gov.companieshouse.kafka.producer.ProducerConfig;
+
+@Configuration
+@Profile("!test")
+public class KafkaConfig {
+
+    private final ChsDeltaDeserializer chsDeltaDeserializer;
+
+    private final String bootstrapServers;
+
+    public KafkaConfig(ChsDeltaDeserializer chsDeltaDeserializer, @Value("${spring.kafka"
+            + ".bootstrap-servers}") String bootstrapServers) {
+        this.chsDeltaDeserializer = chsDeltaDeserializer;
+        this.bootstrapServers = bootstrapServers;
+    }
+
+    /**
+     * Kafka Consumer Factory.
+     */
+    @Bean
+    public ConsumerFactory<String, ChsDelta> kafkaConsumerFactory() {
+        return new DefaultKafkaConsumerFactory<>(consumerConfigs(), new StringDeserializer(),
+                chsDeltaDeserializer);
+    }
+
+    /**
+     * Kafka Listener Container Factory.
+     */
+    @Bean
+    public ConcurrentKafkaListenerContainerFactory<String, ChsDelta> listenerContainerFactory() {
+        ConcurrentKafkaListenerContainerFactory<String, ChsDelta> factory
+                = new ConcurrentKafkaListenerContainerFactory<>();
+        factory.setConsumerFactory(kafkaConsumerFactory());
+        return factory;
+    }
+
+    /**
+     * CH Kafka Producer.
+     */
+    @Bean
+    public CHKafkaProducer chKafkaProducer() {
+        final ProducerConfig config = createProducerConfig();
+        config.setRoundRobinPartitioner(true);
+        config.setAcks(Acks.WAIT_FOR_ALL);
+        config.setRetries(10);
+        config.setRequestTimeoutMilliseconds(3000);
+        return new CHKafkaProducer(config);
+    }
+
+    private Map<String, Object> consumerConfigs() {
+        Map<String, Object> props = new HashMap<>();
+        props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
+        props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+        props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, ChsDeltaDeserializer.class);
+        props.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
+
+        return props;
+    }
+
+    private ProducerConfig createProducerConfig() {
+        final ProducerConfig config = new ProducerConfig();
+        config.setBrokerAddresses(bootstrapServers.split(","));
+        return config;
+    }
+}
